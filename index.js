f25569ab4638f8aabad4162ecd92ab4662cd0b1c
AbhayKaushik <35023708+AbhayKaushik@users.noreply.github.com>
2018-07-08 17:20:28 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
+'use strict';
+
+const {dialogflow} = require('actions-on-google');
+const functions = require('firebase-functions');
+const app = dialogflow({debug: true});
+
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+const hook = new Discord.WebHookClient('weebhook id','webhook token');
+
+app.intent('ENTER THE INTENT NAME', (conv, {ENTER THE PARAMETER NAME}) => {
+    conv.close('Sure,I'll message them on Discord');
+});
+
+hook.send('Let's play PUBG friends!');
 
 client.on("ready", () => {
   console.log("I am ready!");
 });
 
-client.on("message", (message) => {
-  if (!message.content.startsWith(config.prefix) || message.author.bot)
-    return;
-  if (message.content.startsWith(config.prefix + "ping")) {
-    message.channel.send("pong!");
-  }
-});
-
 client.login(config.token);
+
+// Set the DialogflowApp object to handle the HTTPS POST request.
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
+
+
+
+
+
+
+
